f608ec554a646e03dcafc27b0906c3ace73829ac
ZYGA <j.zyznowski123@gmail.com>
2024-07-13 23:50:18 +0200

    a

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ const client = new Client({
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
     IntentsBitField.Flags.GuildVoiceStates,
+    IntentsBitField.Flags.GuildModeration,
   ],
 });
 
@@ -33,21 +34,19 @@ client.on("messageCreate", (message) => {
 
   if (message.author.bot) return;
 
-  if (message.content == '.') {
-    const key = process.env.WEATHER_KEY
-    const baseUrl = "https://api.openweathermap.org/data/2.5/weather?"
-    const city = "Warsaw"
+  if (message.content == ".") {
+    const key = process.env.WEATHER_KEY;
+    const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+    const city = "Ostroleka";
 
-    const URL = baseUrl + "q=" + city + "&appid=" + key
-    console.log(URL)
+    const URL = baseUrl + "q=" + city + "&appid=" + key;
 
+    fetch(URL)
+      .then((result) => result.json())
+      .then((data) => console.log(Math.round(data.main.temp - 273.15)));
 
-    console.log(key)
-
-
-    message.reply('KYS')
+    message.reply("KYS");
   }
-
 
   if (message.content.split(" ")[0] === "sigma") {
     const link = message.content.split(" ")[1];
@@ -71,7 +70,7 @@ client.on("messageCreate", (message) => {
       exec(komenda, (err, out, stderr) => {
         if (err) {
           console.log("komand error");
-          message.reply("zly link kurwo")
+          message.reply("zly link kurwo");
         }
 
         if (stderr) {
@@ -96,10 +95,13 @@ client.on("messageCreate", (message) => {
     }
   }
 
-  if (message.content.toLowerCase() === '!ip'){
-    message.reply('Ip: zyga.icu / 79.184.117.45')
+  if (message.content.toLowerCase() === "!ip") {
+    message.reply("Ip: zyga.icu / 79.184.117.45");
   }
 
+  if (message.content.toLowerCase() === "!wersja") {
+    message.reply("Wersja: 1.21");
+  }
 
   if (message.content.toUpperCase() == "MATHEW GRASZ DUO?") {
     message.reply("KYS");
@@ -111,9 +113,6 @@ client.on("messageCreate", (message) => {
       guildId: message.guild.id,
       adapterCreator: message.guild.voiceAdapterCreator,
     });
-    
-
-
 
     conn.destroy();
   }
@@ -131,9 +130,30 @@ client.on("interactionCreate", (interaction) => {
         "https://th.bing.com/th/id/R.d786da2ec6bfacc6b066f7b80a5e5228?rik=pT24zl1SBLKJtw&riu=http%3a%2f%2fwallsdesk.com%2fwp-content%2fuploads%2f2016%2f12%2fWild-Boar-Wallpapers-HD.jpg&ehk=St5F6QW35D4yqym5OJMKuH628SEHq%2fVpHaq0FKRyasU%3d&risl=&pid=ImgRaw&r=0"
       );
 
-      
-
     interaction.reply({ embeds: [embed] });
+  }
+
+  if (interaction.commandName === "pogoda") {
+    const key = process.env.WEATHER_KEY;
+    const baseUrl = "https://api.openweathermap.org/data/2.5/weather?";
+    const city = "Ciechanow";
+
+    const URL = baseUrl + "q=" + city + "&appid=" + key;
+
+    fetch(URL)
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+
+        const embed = new EmbedBuilder()
+          .setTitle("Pogoda")
+          .setDescription(
+            `${data.name}: ${(data.main.temp - 273.15).toFixed(1)} C`
+          )
+          .setTimestamp();
+
+        interaction.reply({ embeds: [embed] });
+      });
   }
 });
 
